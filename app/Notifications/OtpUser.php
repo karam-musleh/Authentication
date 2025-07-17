8d4fbd9563a641c06f8b031608c99c2756cf73c4
karam-musleh <kk420271025@gmail.com>
2025-07-17 17:48:34 +0300

    Authintication Process

---
+++ app/Notifications/OtpUser.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OtpUser extends Notification
+{
+    use Queueable;
+
+    public $otp;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($otp)
+    {
+        //
+        $this->otp = $otp;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Your OTP Code')
+            ->line('Your One-Time Password (OTP) is: ' . $this->otp)
+            ->line('It will expire in 10 minutes.')
+            ->line('Do not share this code with anyone.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
